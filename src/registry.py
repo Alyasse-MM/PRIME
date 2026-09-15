c6781f5c12c1048488fef5248099ae8f682c4c93
Alyasse-MM <251863836+Alyasse-MM@users.noreply.github.com>
2026-09-15 12:50:38 +0200

    Add exercise list rendering on grade pages

--- src/registry.py
+++ src/registry.py
@@ -1,30 +1,36 @@
 # Dynamically import all exercise classes from the exercises directory and
-# register them in EXERCISE_REGISTRY.
+# register them in grades specific registries.
 
 import importlib
 import inspect
 from pathlib import Path
+import streamlit as st
 
 from exercises.base_exercise import BaseExercise
 
-EXERCISE_REGISTRY_SECONDE = []
-EXERCISE_REGISTRY_PREMIERE = []
-EXERCISE_REGISTRY_TERMINALE = []
+EXERCISE_REGISTRY_SECONDE = {}
+EXERCISE_REGISTRY_PREMIERE = {}
+EXERCISE_REGISTRY_TERMINALE = {}
 
-grades = {"seconde": EXERCISE_REGISTRY_SECONDE,
-          "première": EXERCISE_REGISTRY_PREMIERE,
-          "terminale": EXERCISE_REGISTRY_TERMINALE}
+grades = {
+    "seconde": EXERCISE_REGISTRY_SECONDE,
+    "premiere": EXERCISE_REGISTRY_PREMIERE,
+    "terminale": EXERCISE_REGISTRY_TERMINALE
+}
 
 current_dir = Path(__file__).parent
-exercises_dir = current_dir / "exercises"
 
-for elt, registry in grades.items():
-    exercises_dir = current_dir / "exercises" / elt
+for folder_name, target_dict in grades.items():
+    exercises_dir = current_dir / "exercises" / folder_name
+
+    if not exercises_dir.exists():
+        continue
+
     for file_path in exercises_dir.rglob("*.py"):
         if file_path.name == "__init__.py":
             continue
 
-        relative_path = file_path.relative_to(current_dir.parent)
+        relative_path = file_path.relative_to(current_dir)
         module_name = ".".join(relative_path.with_suffix("").parts)
 
         try:
@@ -33,7 +39,16 @@ for elt, registry in grades.items():
             print(f"Erreur lors du chargement de {module_name}: {e}")
             continue
 
+        chap_fr = getattr(module, "CHAPTER_FR", "Autre")
+        chap_en = getattr(module, "CHAPTER_EN", "Other")
+
+        if chap_fr not in target_dict:
+            if st.session_state.lang == "fr":
+                target_dict[chap_fr] = []
+            else:
+                target_dict[chap_en] = []
+
         for name, obj in inspect.getmembers(module, inspect.isclass):
             if issubclass(obj, BaseExercise) and obj is not BaseExercise:
                 if obj.__module__ == module_name:
-                    registry.append(obj)
+                    target_dict[chap_fr].append(obj)
--- src/views/render.py
+++ src/views/render.py
@@ -1,6 +1,8 @@
+from numpy import size
 import streamlit as st
+from exercises.base_exercise import BaseExercise
 
-def render_grade_page(grade: str, registry: list):
+def render_grade_page(grade_title: str, registry: dict[str, list[type[BaseExercise]]]):
     """
     Renders the page for a specific grade.
 
@@ -9,13 +11,22 @@ def render_grade_page(grade: str, registry: list):
         registry (list): The list of exercises for the grade.
     """
 
-    st.set_page_config(page_title=f"PRIME - {grade.capitalize()}", layout="centered")
+    st.set_page_config(page_title=f"PRIME - {grade_title.capitalize()}", layout="centered")
 
     if "page" not in st.session_state:
-        st.session_state.page = grade
+        st.session_state.page = grade_title
 
-    st.title(f"{grade.capitalize()}")
-    if st.session_state.lang == "fr":
-        st.info("Pas encore implémenté")
+    st.title(f"{grade_title.capitalize()}")
+    if len(registry) == 0:
+        if st.session_state.lang == "fr":
+            st.info("Pas encore implémenté")
+        else:
+            st.info("Not implemented yet")
     else:
-        st.info("Not implemented yet")
+        for chapter, exercises in registry.items():
+            with st.expander(f"{chapter} ({len(exercises)} exercices)", expanded=False):
+                for exercise_class in exercises:
+                    if st.session_state.lang == "fr":
+                        st.write(exercise_class.lang_fr)
+                    else:
+                        st.write(exercise_class.lang_en)
